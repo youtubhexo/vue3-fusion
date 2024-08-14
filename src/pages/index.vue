4221ffb10d3d30b7fa93d0f6bc9c22a3e7971cf8
desperado <youtubhexo@gmail.com>
2024-08-14 17:59:40 +0800

    unplugin-auto-import+vueuse自动导入集成成功

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    {{ msg }}
+    {{ now }}
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+const msg = ref('AI赋能开发')
+const now = useNow()
+</script>
 
 <style lang="scss" scoped></style>
